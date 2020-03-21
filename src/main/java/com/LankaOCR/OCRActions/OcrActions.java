f97d62d5b79a4652a16bce1ba71c77027323695b
Kasun Balasooriya <kmbkck1@gmail.com>
2020-03-21 19:52:58 +0530

    Performing ocr body added

--- src/main/java/com/LankaOCR/OCRActions/OcrActions.java
+++ src/main/java/com/LankaOCR/OCRActions/OcrActions.java
@@ -5,6 +5,13 @@
  */
 package com.LankaOCR.OCRActions;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
 /**
  *
  * @author kasun
@@ -12,8 +19,22 @@ package com.LankaOCR.OCRActions;
 public class OcrActions {
     
     public String PerformOcr(String filePath){
-    String result=null;
     
+     String result = null;   
+     File imageFile = new File(filePath);
+
+        System.out.println(imageFile.getAbsolutePath());
+        Tesseract instance = new Tesseract();// JNA Interface Mapping
+        instance.setLanguage("sin");
+        instance.setHocr(true);
+        instance.setDatapath(".");
+
+        try {
+            result = instance.doOCR(imageFile);
+            
+             } catch (TesseractException e) {
+            System.err.println(e.getMessage());
+        }
     
         return result;
     }
