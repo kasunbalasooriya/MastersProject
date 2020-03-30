3c7cf06cc443a2e58b1309093701c70a532cc997
Kasun Balasooriya <kmbkck1@gmail.com>
2020-03-30 13:47:54 +0530

    consonent normalization implemenatation

--- src/main/java/com/LankaOCR/OCRActions/OcrActions.java
+++ src/main/java/com/LankaOCR/OCRActions/OcrActions.java
@@ -56,6 +56,7 @@ public class OcrActions {
                 innerSpanContent = span.html();
                 innerText = span.text();
                 normalizedInnerText = applyVowelNormalizationRules(innerText); // Apply Vowel Normalization rules
+                normalizedInnerText = applyConsonentNormalizationRules(innerText);
                 innerSpanContent = innerSpanContent.replace(innerText, normalizedInnerText);
                 span.html(innerSpanContent);
 
@@ -74,68 +75,122 @@ public class OcrActions {
 
     private String applyVowelNormalizationRules(String wordString) {
 
-        String modifiedWordString=wordString;
+        String modifiedWordString = wordString;
 
-        /** Sinhala Code point range in decimal 3456-3583 **/
-        
+        /**
+         * Sinhala Code point range in decimal 3456-3583 *
+         */
         // Start Replace the Vowels with modifies to the proper character
-        
         if (wordString.charAt(0) == 3461 && wordString.charAt(1) == 3535) { //          SINHALA LETTER AAYANNA
-            
+
             modifiedWordString = wordString.replace(Character.toString(wordString.charAt(0)), Character.toString((char) 3462));
             StringBuilder tempWordString1 = new StringBuilder(modifiedWordString);
             tempWordString1.deleteCharAt(1);
-            modifiedWordString=tempWordString1.toString();
-            
-        } else if(wordString.charAt(0) == 3461 && wordString.charAt(1) == 3536){ //     SINHALA LETTER AEYANNA
-            
+            modifiedWordString = tempWordString1.toString();
+
+        } else if (wordString.charAt(0) == 3461 && wordString.charAt(1) == 3536) { //     SINHALA LETTER AEYANNA
+
             modifiedWordString = wordString.replace(Character.toString(wordString.charAt(0)), Character.toString((char) 3463));
             StringBuilder tempWordString2 = new StringBuilder(modifiedWordString);
             tempWordString2.deleteCharAt(1);
-            modifiedWordString=tempWordString2.toString();
-        
-        } else if (wordString.charAt(0) == 3461 && wordString.charAt(1) == 3537){ //	SINHALA LETTER AEEYANNA
-        
+            modifiedWordString = tempWordString2.toString();
+
+        } else if (wordString.charAt(0) == 3461 && wordString.charAt(1) == 3537) { //	SINHALA LETTER AEEYANNA
+
             modifiedWordString = wordString.replace(Character.toString(wordString.charAt(0)), Character.toString((char) 3464));
             StringBuilder tempWordString3 = new StringBuilder(modifiedWordString);
             tempWordString3.deleteCharAt(1);
-            modifiedWordString=tempWordString3.toString();
-            
+            modifiedWordString = tempWordString3.toString();
+
         } else if (wordString.charAt(0) == 3467 && wordString.charAt(1) == 3551) { //   SINHALA LETTER UUYANNA
-        
+
             modifiedWordString = wordString.replace(Character.toString(wordString.charAt(0)), Character.toString((char) 3468));
             StringBuilder tempWordString4 = new StringBuilder(modifiedWordString);
             tempWordString4.deleteCharAt(1);
-            modifiedWordString=tempWordString4.toString();
-        
+            modifiedWordString = tempWordString4.toString();
+
         } else if (wordString.charAt(0) == 3545 && wordString.charAt(1) == 3473) { // 	SINHALA LETTER AIYANNA
-        
+
             modifiedWordString = wordString.replace(Character.toString(wordString.charAt(1)), Character.toString((char) 3475));
             StringBuilder tempWordString5 = new StringBuilder(modifiedWordString);
             tempWordString5.deleteCharAt(0);
-            modifiedWordString=tempWordString5.toString();
-            
+            modifiedWordString = tempWordString5.toString();
+
         } else if (wordString.charAt(0) == 3476 && wordString.charAt(1) == 3551) { //  SINHALA LETTER AUYANNA
-        
+
             modifiedWordString = wordString.replace(Character.toString(wordString.charAt(0)), Character.toString((char) 3478));
             StringBuilder tempWordString6 = new StringBuilder(modifiedWordString);
             tempWordString6.deleteCharAt(1);
-            modifiedWordString=tempWordString6.toString();
+            modifiedWordString = tempWordString6.toString();
         }
-        
+
         //TODO : add  follwoing Vowel rules Later 
         /*
         	SINHALA LETTER IRUYANNA
                 SINHALA LETTER IRUUYANNA
         	SINHALA LETTER ILUUYANNA
         
-        */
-        
-        
+         */
         // End Replace the Vowels with modifies to the proper character
-        
-
         return modifiedWordString;
+    }
+
+    private String applyConsonentNormalizationRules(String innerText) {
+
+        // Sinhala Consonent character range between 3482 - 3526
+        int startingChar, middleChar, endChar;
+        char tempChar, firstChar, secondChar, thirdChar;
+        char[] innertTextCharArray = innerText.toCharArray();
+
+        for (int i = 0; i < innertTextCharArray.length -3;) {
+            
+            
+            startingChar = innerText.charAt(i);
+            middleChar = innerText.charAt(i + 1);
+            endChar = innerText.charAt(i + 2);
+            char[] tempCharArray = innerText.toCharArray();
+            
+            log.info(innerText+"- Length ="+innertTextCharArray.length+"  current processing Char : "+ startingChar );
+            
+            if (startingChar > 3535 && startingChar < 3571) {
+                switch (startingChar) {
+
+                    case 3545: //SINHALA VOWEL SIGN KOMBUVA
+
+                        tempChar = tempCharArray[i + 2];
+                        firstChar = tempCharArray[i];
+                        secondChar = tempCharArray[i + 1];
+                        thirdChar = tempCharArray[i + 2];
+
+                        if (middleChar == 3545) { //SINHALA VOWEL SIGN KOMBU DEKA 
+
+                            //switch characters
+                            tempCharArray[i] = secondChar;
+                            tempCharArray[i + 1] = (char) 3547;
+                            String tempString = String.valueOf(tempCharArray);
+                            StringBuilder tempOutputString = new StringBuilder(tempString);
+                            tempOutputString.deleteCharAt(i + 3);
+                            innerText = tempOutputString.toString();
+                            i = i + 3;
+
+                        } else if (middleChar >= 3482 && middleChar <= 3526) {
+
+                            tempCharArray[i] = secondChar;
+                            tempCharArray[i + 1] = (char) firstChar;
+                            innerText = String.valueOf(tempCharArray);
+                            i = i + 2;
+                        }
+                }
+
+            } else {
+                
+                i++;
+
+            }
+
+        }
+
+        return innerText;
     }
 
     private static String escapeNonAscii(String str) {
