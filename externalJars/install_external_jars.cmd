cffa99aafbedc694b24d0fc4312608ac176b955a
Kasun Balasooriya <kmbkck1@gmail.com>
2020-04-11 14:25:16 +0530

    external jars and script to add through maven

---
+++ externalJars/install_external_jars.cmd
@@ -0,0 +1,4 @@
+call mvn install:install-file -Dfile=jai_codec-1.1.3.jar -DgroupId=com.sun.media -DartifactId=jai_codec -Dversion=1.1.3 -Dpackaging=jar
+call mvn install:install-file -Dfile=jai_core-1.1.3.jar -DgroupId=com.sun.media -DartifactId=jai_core -Dversion=1.1.3 -Dpackaging=jar
+
+pause
