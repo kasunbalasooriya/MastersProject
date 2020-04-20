5921e67dc56d082bd3120f9a805414cf99833e98
Kasun Balasooriya <kmbkck1@gmail.com>
2020-04-20 19:04:41 +0530

    Create DiffActions.java

---
+++ src/main/java/com/LankaOCR/actions/DiffActions.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.LankaOCR.actions;
+
+/**
+ *
+ * @author kasun
+ */
+public class DiffActions {
+    
+ protected void createDiffReport(String orignalText, String ocrText){
+ 
+     //TODO implement method to diff the original vs output
+ 
+ } 
+    
+}
